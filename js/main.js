ca3890224d43efd522f65db19a857a9536f3a218
Abdullah Miraz <abdullahmiraz2015@gmail.com>
2023-06-08 16:31:59 +0600

    test

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const loadNews = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNews(data.data);
+}
+const displayNews = (newsList) => {
+    const newsContainer = document.getElementById('news-container');
+    newsList.forEach(news => {
+        
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col');
+        newsDiv.innerHTML = `
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="./images/bbc.png" class="img-fluid w-100 h-100" style="object-fit: cover;"
+                            alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins
+                                    ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ` ;
+
+        newsContainer.appendChild(newsDiv);
+    });
+
+}
+
+
+
+
+loadNews();
